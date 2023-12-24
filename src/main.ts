9f89908af6872abcfd92bddb0f0a9a4726caeacd
Mahmoud Salah <65346053+DresdRam@users.noreply.github.com>
2023-12-24 16:10:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './model/app/app.module';
 
-const port: number = 3026;
+const port: number = 3028;
 const successMessage: string = `API Started Listening On Port ${port} ....`;
 
 async function bootstrap() {
