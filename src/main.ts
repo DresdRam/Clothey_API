2f7639a20f16409a413aa72864a41f0d0d127ddc
Mahmoud Salah <65346053+DresdRam@users.noreply.github.com>
2023-12-24 16:28:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './model/app/app.module';
 
-const port: number = 3028;
+const port: number = process.env.PORT || 3026;
 const successMessage: string = `API Started Listening On Port ${port} ....`;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(port).then(() => {
+  await app.listen(port, "0.0.0.0").then(() => {
     console.log(successMessage);
   }).catch((error: any) => {
     console.log(error.message);
