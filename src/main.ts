5a6955072c486231554f64fec63c113ea5a46410
DresdRam <snipermayv@gmail.com>
2023-12-27 17:30:07 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './model/app/app.module';
 
-const developement: number = 1;
+const developement: number = 0;
 let port: number;
 let host: string;
 
@@ -17,13 +17,8 @@ if (developement == 0) {
 const successMessage: string = `API Started Listening On ${host}:${port} ....`;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
   await app.listen(port, host).then(() => {
     console.log(successMessage);
   }).catch((error: any) => {
